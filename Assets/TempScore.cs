44e1989b1446fef87be07a4e8d3fe45d40e027c5
athakaspen <qwertydave54@gmail.com>
2022-04-08 17:50:15 -0500

    Added score system to Utilities, along with a temporary UI display

---
+++ Assets/TempScore.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempScore : MonoBehaviour
+{
+    public Text myText;
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = Utilities.score.ToString();
+    }
+}
